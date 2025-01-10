43c465965e632dd26cbb1147adb60aa7121c0d49
Amogh <amogh.maheshwari@email.com>
2025-01-10 11:55:19 -0800

    adding hoots functionality added with form component

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,9 +9,10 @@ const NavBar = ({ handleSignout }) => {
       {user ? (
         <nav>
             <ul>
-              <li><Link to='/'>HOME</Link></li>
-              <li><Link to='/hoots'>HOOTS</Link></li>
-              <li><Link to='' onClick={handleSignout}>SIGN OUT</Link></li>
+              <li><Link to = '/'>HOME</Link></li>
+              <li><Link to = '/hoots'>HOOTS</Link></li>
+              <li><Link to = '' onClick={handleSignout}>SIGN OUT</Link></li>
+              <li> <Link to = "/hoots/new">NEW HOOT</Link> </li>
             </ul>
         </nav>
       ) : (
--- src/services/hootService.js
+++ src/services/hootService.js
@@ -22,6 +22,22 @@ const show = async (hootId) => {
       console.log(error);
     }
   };
+
+  const create = async (hootFormData) => {
+    try {
+      const res = await fetch(BASE_URL, {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(hootFormData),
+      });
+      return res.json();
+    } catch (error) {
+      console.log(error);
+    }
+  };
   
   
-  export { index, show};
+  export { index, show, create};
