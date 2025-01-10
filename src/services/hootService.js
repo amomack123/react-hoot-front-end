2e08eb2934f994583444e3a49178b2ae5958cd0f
Amogh <amogh.maheshwari@email.com>
2025-01-10 11:06:24 -0800

    HootDetails component to display information for a single hoot given id

--- src/services/hootService.js
+++ src/services/hootService.js
@@ -11,5 +11,17 @@ const index = async () => {
       console.log(error);
     }
 };
+
+const show = async (hootId) => {
+    try {
+      const res = await fetch(`${BASE_URL}/${hootId}`, {
+        headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+      });
+      return res.json();
+    } catch (error) {
+      console.log(error);
+    }
+  };
   
-  export { index };
+  
+  export { index, show};
