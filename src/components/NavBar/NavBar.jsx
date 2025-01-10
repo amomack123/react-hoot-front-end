f99bb3975f70b781869527ccf96104ca956a94f2
Amogh <amogh.maheshwari@email.com>
2025-01-10 10:24:46 -0800

    Add hoot service and HootList component for displaying and linking hoots

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,17 +8,11 @@ const NavBar = ({ handleSignout }) => {
     <>
       {user ? (
         <nav>
-          <ul>
-            <li>Welcome, {user.username}</li>
-            <li>
-              <Link to="/">Dashboard</Link>
-            </li>
-            <li>
-              <Link to="" onClick={handleSignout}>
-                Sign Out
-              </Link>
-            </li>
-          </ul>
+            <ul>
+              <li><Link to='/'>HOME</Link></li>
+              <li><Link to='/hoots'>HOOTS</Link></li>
+              <li><Link to='' onClick={handleSignout}>SIGN OUT</Link></li>
+            </ul>
         </nav>
       ) : (
         <nav>
@@ -36,3 +30,43 @@ const NavBar = ({ handleSignout }) => {
   );
 };
 export default NavBar;
+
+// import { Link } from 'react-router-dom';
+// import { AuthedUserContext } from '../../App';
+// import { useContext } from 'react';
+
+// const NavBar = ({ handleSignout }) => {
+//   const user = useContext(AuthedUserContext);
+//   return (
+//     <>
+//       {user ? (
+//         <nav>
+//           <ul>
+//             <li>Welcome, {user.username}</li>
+//             <li>
+//               <Link to="/">Dashboard</Link>
+//             </li>
+//             <li>
+//               <Link to="" onClick={handleSignout}>
+//                 Sign Out
+//               </Link>
+//             </li>
+//           </ul>
+//         </nav>
+//       ) : (
+//         <nav>
+//           <ul>
+//             <li>
+//               <Link to="/signin">Sign In</Link>
+//             </li>
+//             <li>
+//               <Link to="/signup">Sign Up</Link>
+//             </li>
+//           </ul>
+//         </nav>
+//       )}
+//     </>
+//   );
+// };
+// export default NavBar;
+
